305104b163735e0a97b0ddd3d6e6ca037f64158c
Emperor Starfinder <emperor@secondgalaxy.com>
2024-03-16 16:42:49 -0400

    Cleanup Addons.OfflineIM
    - Fix license text to prevent false errors being detected by security scanning systems
    - Cleanup commented out code
    - Cleanup formatting
    - Cleanup whitespaces
    - Cleanup code

--- OpenSim/Addons/OfflineIM/Remote/OfflineIMServiceRemoteConnector.cs
+++ OpenSim/Addons/OfflineIM/Remote/OfflineIMServiceRemoteConnector.cs
@@ -1,44 +1,56 @@
-﻿/*
- * Copyright (c) Contributors, http://opensimulator.org/
- * See CONTRIBUTORS.TXT for a full list of copyright holders.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the OpenSimulator Project nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+﻿/// <license>
+/// Copyright (c) Contributors, http://opensimulator.org/
+/// See CONTRIBUTORS.TXT for a full list of copyright holders.
+///
+/// Redistribution and use in source and binary forms, with or without
+/// modification, are permitted provided that the following conditions are met:
+///     * Redistributions of source code must retain the above copyright
+///       notice, this list of conditions and the following disclaimer.
+///     * Redistributions in binary form must reproduce the above copyright
+///       notice, this list of conditions and the following disclaimer in the
+///       documentation and/or other materials provided with the distribution.
+///     * Neither the name of the OpenSimulator Project nor the
+///       names of its contributors may be used to endorse or promote products
+///       derived from this software without specific prior written permission.
+///
+/// THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+/// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+/// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+/// DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+/// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+/// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+/// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+/// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+/// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+/// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// </license>
 
+/// <summary>
+/// System Library Using 
+/// References First
+/// </summary>
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 
+/// <summary>
+/// Platform Library Using 
+/// References 
+/// </summary>
 using OpenSim.Framework;
 using OpenSim.Framework.ServiceAuth;
 using OpenSim.Server.Base;
 using OpenSim.Services.Interfaces;
 
-using OpenMetaverse;
+/// <summary>
+/// Additional Third Party 
+/// Library Using References
+/// </summary>
 using log4net;
 using Nini.Config;
+using OpenMetaverse;
 
 namespace OpenSim.OfflineIM
 {
@@ -59,6 +71,7 @@ namespace OpenSim.OfflineIM
         public OfflineIMServiceRemoteConnector(IConfigSource config)
         {
             IConfig cnf = config.Configs["Messaging"];
+
             if (cnf == null)
             {
                 m_log.WarnFormat("[OfflineIM.V2.RemoteConnector]: Missing Messaging configuration");
@@ -67,7 +80,7 @@ namespace OpenSim.OfflineIM
 
             m_ServerURI = cnf.GetString("OfflineMessageURL", string.Empty);
 
-            /// This is from BaseServiceConnector
+            // This is from BaseServiceConnector
             string authType = Util.GetConfigVarFromSections<string>(config, "AuthType", new string[] { "Network", "Messaging" }, "None");
 
             switch (authType)
@@ -76,12 +89,13 @@ namespace OpenSim.OfflineIM
                     m_Auth = new BasicHttpAuthentication(config, "Messaging");
                     break;
             }
-            ///
+
             m_log.DebugFormat("[OfflineIM.V2.RemoteConnector]: Offline IM server at {0} with auth {1}", 
                 m_ServerURI, (m_Auth == null ? "None" : m_Auth.GetType().ToString()));
         }
 
         #region IOfflineIMService
+
         public List<GridInstantMessage> GetMessages(UUID principalID)
         {
             List<GridInstantMessage> ims = new List<GridInstantMessage>();
@@ -91,12 +105,17 @@ namespace OpenSim.OfflineIM
             Dictionary<string, object> ret = MakeRequest("GET", sendData);
 
             if (ret == null)
+            {
                 return ims;
+            }
 
             if (!ret.ContainsKey("RESULT"))
+            {
                 return ims;
+            }
 
             string result = ret["RESULT"].ToString();
+
             if (result == "NULL" || result.ToLower() == "false")
             {
                 string reason = ret.ContainsKey("REASON") ? ret["REASON"].ToString() : "Unknown error";
@@ -127,6 +146,7 @@ namespace OpenSim.OfflineIM
             }
 
             string result = ret["RESULT"].ToString();
+
             if (result == "NULL" || result.ToLower() == "false")
             {
                 reason = ret.ContainsKey("REASON") ? ret["REASON"].ToString() : "Unknown error";
@@ -146,7 +166,6 @@ namespace OpenSim.OfflineIM
 
         #endregion
 
-
         #region Make Request
 
         private Dictionary<string, object> MakeRequest(string method, Dictionary<string, object> sendData)
@@ -154,18 +173,20 @@ namespace OpenSim.OfflineIM
             sendData["METHOD"] = method;
 
             string reply = string.Empty;
+
             lock (m_Lock)
+            {
                 reply = SynchronousRestFormsRequester.MakeRequest("POST",
                          m_ServerURI + "/offlineim",
                          ServerUtils.BuildQueryString(sendData),
                          m_Auth);
+            }
 
-            Dictionary<string, object> replyData = ServerUtils.ParseXmlResponse(
-                    reply);
+            Dictionary<string, object> replyData = ServerUtils.ParseXmlResponse(reply);
 
             return replyData;
         }
-        #endregion
 
+        #endregion
     }
 }
